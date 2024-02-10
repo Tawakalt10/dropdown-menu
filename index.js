57ef4ad77f6d04f3cb4cc1c52dd92f422c6e8e32
unknown <Tawakalt10>
2024-02-10 09:36:10 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// ===========DROPDOWN JS==========//
+const showDropdown = (content, button)=>{
+    const dropdownContent = document.getElementById(content),
+    dropdownButton = document.getElementById(button)
+
+    dropdownButton.addEventListener('click',()=>{
+        // we add the show-dropdown class, so that the menu is displayed
+        dropdownContent.classList.toggle('show-dropdown')
+    })
+} 
+
+ showDropdown('dropdown-content', 'dropdown-button')
